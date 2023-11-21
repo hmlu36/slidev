5470c67b50c8a80c95ff71144d877dee0838c354
a0937370704 <a0937370704@gmail.com>
2023-11-21 17:50:49 +0800

    滾輪及點擊換頁

---
+++ global-top.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="scroll-area absolute bottom-0 left-0 right-0 p-2"  @wheel="scrollPage" @click="$slidev.nav.next"></div>
+</template>
+
+<script setup lang="ts">
+
+
+const scrollPage = (e: { deltaY: number }) => {
+  if (e.deltaY > 0) {
+    $slidev.nav.next();
+  } else {
+    $slidev.nav.prev();
+  }
+}
+
+</script>
+
+<style scoped>
+
+.scroll-area {
+  width: 100%;
+  height: 100vh;
+}
+
+</style>
