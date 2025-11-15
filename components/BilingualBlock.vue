860e09aea117b19ffb3ed5de904fc7f68c5bf088
Joshua <hmlu36@gmail.com>
2025-11-15 14:46:18 +0800

    新增BilingualBlock, 調整H1縮排

---
+++ components/BilingualBlock.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="bilingual">
+    <slot />
+  </div>
+</template>
+
+<style scoped>
+.bilingual {
+  text-align: center;
+  margin: 0.6rem 0;
+  letter-spacing: 0;
+}
+
+</style>
